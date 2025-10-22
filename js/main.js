1d944e565ee3ba516b286017f162efe463929fc0
Mateus-Cimini <mateusmateus122333@gmail.com>
2025-10-22 02:26:27 -0300

    fix: variaveis css para definir cores do projeto

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 const body = document.body;
 const btn = document.getElementById('toggleTheme');
+const icon = document.getElementById('iconTheme');
 
-const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-if (prefersDark) body.classList.add('dark')
 
 btn.onclick = () => {
   body.classList.toggle('dark')
+  if(icon.classList.contains('bi-sun')){
+    icon.className = 'bi bi-moon-stars'
+  } else {
+    icon.className = 'bi bi-sun' 
+  }
+
+  if(btn.classList.contains('btn-light')) {
+    btn.className = 'btn btn-dark'
+  } else {
+    btn.className = 'btn btn-light'
+  }
 };
