4396ade3526972526a43fae8050a234376d98009
Mateus-Cimini <mateusmateus122333@gmail.com>
2025-12-31 20:18:10 -0300

    feat: adicionando efeitos e animaçoes

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
 const body = document.body;
-const btn = document.getElementById('toggleTheme');
-const icon = document.getElementById('iconTheme');
-
+const btn = document.getElementById("toggleTheme");
+const icon = document.getElementById("iconTheme");
 
 btn.onclick = () => {
-  body.classList.toggle('dark')
-  if(icon.classList.contains('bi-sun')){
-    icon.className = 'bi bi-moon-stars'
+  body.classList.toggle("dark");
+  if (icon.classList.contains("bi-sun")) {
+    icon.className = "bi bi-moon-stars";
   } else {
-    icon.className = 'bi bi-sun' 
+    icon.className = "bi bi-sun";
   }
 
-  if(btn.classList.contains('btn-light')) {
-    btn.className = 'btn btn-dark'
+  if (btn.classList.contains("btn-light")) {
+    btn.className = "btn btn-dark";
   } else {
-    btn.className = 'btn btn-light'
+    btn.className = "btn btn-light";
   }
 };
+
+const modalAddImage = document.getElementById("modalAddImage");
+modalAddImage.addEventListener("hidden.bs.modal", () => {
+  document.getElementById("home").scrollIntoView({ behavior: "smooth" });
+  console.log('rolou');
+  
+});
